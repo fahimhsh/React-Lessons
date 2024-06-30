f53bdd1f099be1e4584b7478118069be92ecf53b
fahimhsh <fbchalai.420@gmail.com>
2024-06-30 07:34:13 +0600

    List & Keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Clock from "./Components/Clock";
+import ClockList from "./Components/ClockList";
 
 class App extends React.Component {
   render() {
+    const quantities = [1, 2, 3];
     return (
       <>
-        <Clock />
+        <ClockList quantities={quantities} />
       </>
     );
   }
---
+++ src/Components/ClockList.jsx
@@ -0,0 +1,13 @@
+import Clock from "./Clock";
+
+function ClockList({ quantities }) {
+  return (
+    <>
+      {quantities.map((key) => (
+        <Clock key={key} />
+      ))}
+    </>
+  );
+}
+
+export default ClockList;
