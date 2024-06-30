22201e3d9d7ee79463c16a6a1ca6e7b8a2e6105b
fahimhsh <fbchalai.420@gmail.com>
2024-06-30 15:53:29 +0600

    Form Handling through React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Clock from "./Components/Clock";
 import ClockList from "./Components/ClockList";
+import Form from "./Components/Form";
 
 class App extends React.Component {
   render() {
     const quantities = [1, 2, 3];
     return (
       <>
-        <ClockList quantities={quantities} />
+        <Form />
       </>
     );
   }
