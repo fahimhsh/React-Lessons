81ba7a328ab1226604c664098b8c75a39846bf78
fahimhsh <fbchalai.420@gmail.com>
2024-07-28 03:11:37 +0600

    useState with Object/Array

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import Todo from "./Components/Todo";
 
-// This file is useless in current branch of this Repo
-// All the codes are written in main.jsx
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <Todo />
             </div>
         );
     }
