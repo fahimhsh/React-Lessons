e720b6dddba72939926aa086f784050f3ee59903
fahimhsh <fbchalai.420@gmail.com>
2024-07-28 02:50:28 +0600

    Created a custom useState hook on our own

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Todo from "./Components/Todo";
 
+// This file is useless in current branch of this Repo
+// All the codes are written in main.jsx
 export default class App extends React.Component {
     render() {
         return (
             <div>
-                <Todo />
+                <h1>Hello</h1>
             </div>
         );
     }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,59 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Custom useState hook
+let states = [];
+let stateIndex = -1;
+
+const useState = (defaultValue) => {
+    const index = ++stateIndex;
+
+    if (states[index]) return states[index];
+
+    const setValue = (newValue) => {
+        states[index][0] = newValue;
+        daddysRendering();
+    };
+
+    const returnArray = [defaultValue, setValue];
+    states[index] = returnArray;
+    return returnArray;
+};
+
+const App = () => {
+    const [todo, setTodo] = useState("");
+    const [warning, setWarning] = useState(null);
+
+    const handleInput = (e) => {
+        const inputValue = e.target.value;
+        const warningMessage = inputValue.includes(".js")
+            ? "You need to learn JS"
+            : null;
+
+        setTodo(inputValue);
+        setWarning(warningMessage);
+    };
+
+    return (
+        <div>
+            <p>{todo}</p>
+            <p>
+                <textarea
+                    name="todo"
+                    value={todo}
+                    onChange={handleInput}
+                ></textarea>
+            </p>
+            <hr />
+            <h2>{warning || "Good Choice!"}</h2>
+        </div>
+    );
+};
+
+function daddysRendering() {
+    stateIndex = -1;
+    ReactDOM.render(<App />, document.getElementById("root"));
+}
+
+// Initial render
+daddysRendering();
